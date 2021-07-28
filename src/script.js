8f7a65cfe911e8da6cd995d09a0901aac3724f53
LuigiImVector <luigi.vector15@virgilio.it>
2021-07-28 12:26:19 +0200

    Aggiunti tutti gli hover + Ripulite regole inutili dal breakpoint per smartphone/tablet + Aggiunto pulsante Exit nel popup di inserimento testo (va migliorato)

--- src/script.js
+++ src/script.js
@@ -1,11 +1,9 @@
 var textArea = document.getElementsByTagName("textarea")[0];
 var saveButton = document.getElementById("save-text");
 
-var text;
-
 /* Edit text button */
 document.getElementsByClassName("side-button")[0].onclick = function () {
-    document.getElementById("input-text").style.top = "50%";
+    document.getElementById("input-text-popup").style.top = "50%";
 }
 
 textArea.addEventListener("input", function(event) {
@@ -21,20 +19,37 @@ textArea.addEventListener("input", function(event) {
 
 saveButton.onclick = function () {
     if(textArea.value.length > 0) {
-        text = textArea.value;
+        localStorage.setItem("text", textArea.value);
 
-        var tmp = text;
+        document.getElementById("text").innerHTML = localStorage.getItem("text").slice(0, 10) + "...";
 
-        document.getElementById("text").innerHTML = tmp.slice(0, 10) + "...";
+        document.getElementById("submit").classList.add("enabled-button");
+        document.getElementById("submit").classList.remove("disabled-button");
 
-        document.getElementById("input-text").style.top = "-50%";
+        document.getElementById("input-text-popup").style.top = "-50%";
     }
+}
+
+document.getElementById("exit").onclick = function () {
+    if(textArea.value.length <= 0) {
+        document.getElementById("submit").classList.add("disabled-button");
+        document.getElementById("submit").classList.remove("enabled-button");
+    }
+
+    document.getElementById("input-text-popup").style.top = "-50%";
 }
 
 window.addEventListener("load", function() {
     if(textArea.value.length>0)
     {
+        document.getElementById("text").innerHTML = localStorage.getItem("text").slice(0, 10) + "...";
+
         saveButton.classList.add("enabled-button");
         saveButton.classList.remove("disabled-button");
+
+        document.getElementById("submit").classList.add("enabled-button");
+        document.getElementById("submit").classList.remove("disabled-button");
     }
+
+    /* Aggiungere update WPM */
 })
