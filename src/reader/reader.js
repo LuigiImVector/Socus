b1d84d12cd5a3269b1b83fa6d0007b05e9d8632b
LuigiImVector <luigi.vector15@virgilio.it>
2021-08-01 19:42:51 +0200

    Finito il reader (con aumento graduale della velocità)

--- src/reader/reader.js
+++ src/reader/reader.js
@@ -1,46 +1,47 @@
 var interval = 0;
-var wpmInitial = 0;
-var i = 0;
+var wpmInitial = 0; /* serve a poter raggiungere gradualmente i WPM scelti dall'utente */
+var wordCounter = 0;
+var text, wpm;
 
+function startInterval (interval) {
+    
+    var timer = setInterval(function () {
+        if(wordCounter<text.length) {
+            document.getElementById("word").innerHTML = text[wordCounter];
+            wordCounter++;
+        } else {
+            clearInterval(timer);
+        }
+
+        // Ogni 5 parole aumenta la velocità di 50 WPM
+        if(wordCounter%5==0) {
+            if(wpmInitial<wpm) {
+                wpmInitial += 50;
+                interval = (60000/wpmInitial);
+                clearInterval(timer);
+                startInterval(interval);
+            }
+        }
+    }, interval);
+
+}
 
 window.addEventListener("load", function () {
     if ((localStorage.getItem("text") != null && localStorage.getItem("text").length>0) && (localStorage.getItem("wpm") != null)) {
 
-        var text = localStorage.getItem("text");
-        var wpm = localStorage.getItem("wpm");
+        text = localStorage.getItem("text");
+        wpm = localStorage.getItem("wpm");
 
         text = text.split(" ");
-
-        console.log(text);
-
-        console.log("Array: " + text.length);
-        console.log("Contatore: " + i);
-        console.log("Interval: " + interval);
-        console.log("wpmInitial: " + wpmInitial);
-        console.log("-----------")
-
-        while(i<text.length) {
-            console.log("Contatore: " + i);
-            console.log("Interval: " + interval);
-            console.log("wpmInitial: " + wpmInitial);
-            console.log("-----------")
-
-            document.getElementById("word").innerHTML = text[i];
-
-            if(i%5 == 0) {
-                if(wpmInitial < wpm) {
-                    wpmInitial += 50;
-                    interval = (60000/wpmInitial);
-                }
-            }
-                
-            setTimeout(function () {
-                
-            }, interval);
-            i++;
-        }
         
-        
+                
+        if(wpm<=200)
+            wpmInitial=50;
+        else
+            wpmInitial=150;
+
+        interval = (60000/wpmInitial);
+        startInterval(interval);
 
     }
 })
